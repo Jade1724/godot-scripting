f552befb3cbfad267f27421fcda84a7ed26f95ad
Jade1724 <haruka.i1724@gmail.com>
2022-03-17 16:47:26 +1300

    Wrote a script to rotate a sprite constantly

--- Sprite.gd
+++ Sprite.gd
@@ -1,5 +1,10 @@
 extends Sprite
 
-func _init():
-	print("Hello World!")
+var speed = 400
+var angular_speed = PI # Godot angle is in radians by default
 
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+	
