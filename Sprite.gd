f39565b9ec934373ff879475fd09df432777e316
Jade1724 <haruka.i1724@gmail.com>
2022-03-17 17:46:14 +1300

    Added script based connection between the Sprite and Timer. Added signal in the Sprite to emit death state

--- Sprite.gd
+++ Sprite.gd
@@ -1,5 +1,8 @@
 extends Sprite
 
+signal health_depleted
+
+var health = 10
 var speed = 400
 var angular_speed = PI # Godot angle is in radians by default
 
@@ -10,3 +13,15 @@ func _process(delta: float) -> void:
 
 func _on_Button_pressed():
 	set_process(not is_processing()) # Replace with function body.
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.connect("timeout", self, "_on_Timer_timeout")
+	
+func _on_Timer_timeout():
+	visible = not visible
+
+func take_damage(amount):
+	health -= amount
+	if health <= 0:
+		emit_signal("health_depleted")
