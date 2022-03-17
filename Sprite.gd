c67caa68a3223f156b719a016e6ec81b00b3d5a5
Jade1724 <haruka.i1724@gmail.com>
2022-03-17 17:32:25 +1300

    Added a toggle motion button with signal to the sprite

--- Sprite.gd
+++ Sprite.gd
@@ -3,18 +3,10 @@ extends Sprite
 var speed = 400
 var angular_speed = PI # Godot angle is in radians by default
 
-func _process(delta):
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-		
-	rotation += angular_speed* direction * delta
-	
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-	
+func _process(delta: float) -> void:
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
-	
+
+func _on_Button_pressed():
+	set_process(not is_processing()) # Replace with function body.
